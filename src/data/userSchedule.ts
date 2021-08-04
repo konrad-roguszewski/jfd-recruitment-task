2e325e9f5aefb13b7d04d907dd3b727cbc98fafd
Konrad Roguszewski <aker.hm@gmail.com>
2021-08-04 16:57:39 +0200

    feat: Added user schedule data

---
+++ src/data/userSchedule.ts
@@ -0,0 +1,89 @@
+export const userSchedule = {
+  dietStartDate: '2021-01-01',
+  dietWeeks: [
+    {
+      name: 'Week',
+      sequence: 1,
+      isWeekDone: true
+    },
+    {
+      name: 'Week',
+      sequence: 2,
+      isWeekDone: true
+    },
+    {
+      name: 'Week',
+      sequence: 3,
+      isWeekDone: true
+    },
+    {
+      name: 'Week',
+      sequence: 4,
+      isWeekDone: true
+    },
+    {
+      name: 'Week',
+      sequence: 5,
+      isWeekDone: true
+    },
+    {
+      name: 'Week',
+      sequence: 6,
+      isWeekDone: true
+    },
+    {
+      name: 'Week',
+      sequence: 7,
+      isWeekDone: false
+    },
+    {
+      name: 'Week',
+      sequence: 8,
+      isWeekDone: false
+    },
+    {
+      name: 'Week',
+      sequence: 9,
+      isWeekDone: false
+    },
+    {
+      name: 'Week',
+      sequence: 10,
+      isWeekDone: false
+    },
+    {
+      name: 'Week',
+      sequence: 11,
+      isWeekDone: false
+    },
+    {
+      name: 'Week',
+      sequence: 12,
+      isWeekDone: false
+    }
+  ],
+  dietDays: [1, 2, 3, 4, 5, 6],
+  dietHours: [
+    {
+      hour: '6:00',
+      suffix: 'am'
+    },
+    {
+      hour: '9:00',
+      suffix: 'am'
+    },
+    {
+      hour: '12:00',
+      suffix: 'pm'
+    },
+    {
+      hour: '3:00',
+      suffix: 'pm'
+    },
+    {
+      hour: '6:00',
+      suffix: 'pm'
+    }
+  ],
+  dietProteinOptions: ['vegetables', 'dairy', 'beef', 'fish', 'poultry']
+};
